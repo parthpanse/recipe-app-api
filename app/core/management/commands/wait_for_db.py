bca8814268bb341f43e9421bc9d139af53d40b1e
parthpanse <parthpanse050401@gmail.com>
2025-07-09 23:39:08 -0700

    Linting

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,12 +1,23 @@
 """
 Wait command for the database to be ready before starting the application.
 """
-
+import time
+from psycopg2 import OperationalError as Psycopg2OpError
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Wait for the database to be ready."""
     def handle(self, *args, **options):
-        pass
-
-    
+        """Entry point for command."""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while not db_up:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database available!'))
